f3328460c23221e75b7c92f1f90bbbe652d79e13
martinbakos99-droid <martin.bakos99@gmail.com>
2026-09-16 20:18:34 +0200

    Update MobileTWAttackSender.js

--- MobileTWAttackSender.js
+++ MobileTWAttackSender.js
@@ -1,20 +1,12 @@
-(() => {
-    const ALLOWED_IDS = [829169];
+// === Ochrana podľa player ID ===
+const ALLOWED_IDS = [829169, 949172]; // sem daj všetky povolené ID
+const playerId = game_data?.player?.id || null;
 
-    const playerId = Number(
-        window.game_data?.player?.id
-    );
-
-    console.log("PLAYER ID:", playerId);
-    console.log("ALLOWED:", ALLOWED_IDS.includes(playerId));
-
-    if (!Number.isInteger(playerId) || !ALLOWED_IDS.includes(playerId)) {
-        alert(
-            "Skript nie je povolený pre tento účet.\n\n" +
-            "Player ID: " + playerId
-        );
-        return;
-    }
+if (!ALLOWED_IDS.includes(Number(playerId))) {
+    alert('Skript nie je povolený pre tento účet. Kontaktuj ma na discorde: CaptainM4rtin :)');
+    throw new Error('Unauthorized');
+}
+// === koniec ochrany ===
 
     let inputMs;
     let input;
